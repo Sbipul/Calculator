a50791c1dba48bddb67ffc8d3a93b01dec1b8d12
Bipul-Ch-Roy <kumarroy.kb.kb@gmail.com>
2021-08-20 07:57:37 +0600

    edited

--- script.js
+++ script.js
@@ -14,10 +14,14 @@ if (minute < 10) {
 if (hour >= 12 && hour <= 23) {
     ampm.innerText = 'PM';
     clockHour.innerText = hour - 12;
+    const day = document.getElementById('day');
+    day.innerText = 'Good after Noon';
 
 } else {
     ampm.innerText = 'AM';
     clockHour.innerText = hour;
+    const day = document.getElementById('day');
+    day.innerText = 'Good Morning';
 }
 document.getElementById('all').addEventListener('click', function (e) {
     var number = e.target.innerText;
@@ -36,9 +40,18 @@ document.getElementById('all').addEventListener('click', function (e) {
         } else if (number == '-') {
             const display = document.getElementById('display');
             display.innerText += '-';
+        } else if (number == '00') {
+            const display = document.getElementById('display');
+            display.innerText += '00';
         } else if (number == 'x') {
             const display = document.getElementById('display');
             display.innerText += '*';
+        } else if (number == '(') {
+            const display = document.getElementById('display');
+            display.innerText += '(';
+        } else if (number == ')') {
+            const display = document.getElementById('display');
+            display.innerText += ')';
         } else if (number == '/') {
             const display = document.getElementById('display');
             display.innerText += '/';
@@ -55,6 +68,35 @@ document.getElementById('all').addEventListener('click', function (e) {
 document.getElementById('delete').addEventListener('click', function () {
     const displayNumber = document.getElementById('display');
     let displayPre = displayNumber.innerText;
-    displayPre = displayPre.substring(0,displayPre.length - 1);
+    displayPre = displayPre.substring(0, displayPre.length - 1);
     displayNumber.innerText = displayPre;
 })
+
+
+
+function back() {
+    const first = document.getElementsByClassName('color-yellow');
+    const bg = document.getElementById('bg-dark');
+    const day = document.getElementById('day');
+    const colorPink = document.getElementsByClassName('color-pink');
+    const textDark = document.getElementById('textDark');
+    const bgbtn = document.getElementsByClassName('bg');
+    textDark.style.color = 'black';
+    day.style.color = 'black';
+    bg.style.background = 'rgb(180, 180, 180)';
+    for (const bgs of bgbtn) {
+        bgs.style.background = 'rgb(204, 204, 204)';
+        bgs.style.color = 'rgb(0, 0, 0)';
+        bgs.style.borderRadius = '.6rem';
+        bgs.style.color = 'rgb(0, 0, 0)';
+    }
+    for (const two of colorPink) {
+        two.style.background = 'rgb(99, 98, 108)';
+        two.style.color = 'white';
+    }
+    for (const one of first) {
+        one.style.background = 'rgb(102, 39, 39)';
+        one.style.color = 'white';
+    }
+
+}
